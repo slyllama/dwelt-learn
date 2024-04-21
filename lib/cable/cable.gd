2947959e3841f814d118f49e9e0a6492eb3f2ef4
Alex Laing <alex.laing@me.com>
2024-04-22 10:37:25 +1200

    Laser cable applied to cable

--- lib/cable/cable.gd
+++ lib/cable/cable.gd
@@ -1,17 +1,24 @@
 extends CSGCylinder3D
 
-var start = Vector3(-17.0, 5.0, -2.3)
+var start = Vector3(-16.0, 6.0, 5.0)
+var active = false
+
+func set_active(get_active):
+	active = get_active
+	if active == true: visible = true
+	else: visible = false
 
 func _ready():
-	pass
+	set_active(false)
 
 func _process(_delta):
+	if active == false: return
 	var end = Global.player_position + Vector3(0, -0.45, 0)
-	position = (start + end) / 2.0
+	global_position = (start + end) / 2.0
 	look_at(end, Vector3.UP)
 	rotation_degrees.x += 90.0
 	
-	height = position.distance_to(end) * 2.0
+	height = global_position.distance_to(end) * 2.0
 	$Sparks.look_at(end, Vector3.UP)
 	$Sparks.position.y = height / 2.0
 	$Glow.look_at(end, Vector3.UP)
--- lib/laser/laser.gd
+++ lib/laser/laser.gd
@@ -11,18 +11,21 @@ func _input(_event):
 	if Input.is_action_just_pressed("interact"):
 		if active == false:
 			active = true
+			$Cable.set_active(true)
 			Global.emit_signal(
 				"player_position_locked",
 				Vector3(position.x, 4.0, position.z),
 				Vector2(90.0, 10.0), 40.0, 20.0)
 			return
 		else:
-			Global.interact_left.emit() # hide overlay on leaving
+			$Cable.set_active(false)
 			active = false
+			Global.interact_left.emit() # hide overlay on leaving
 			Global.player_position_unlocked.emit()
 			return
 
 func _physics_process(_delta):
+	$Cable.position.x -= 0.01
 	if Global.in_area_name == TYPE:
 		if in_area == false: in_area = true
 	else:
