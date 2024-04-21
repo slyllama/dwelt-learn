f2eb635f6c280911d2c27a1f6ae81a7b2e235de0
Alex Laing <alex.laing@me.com>
2024-04-21 15:29:44 +1200

    Tested propulsion particles

--- test_scene.gd
+++ test_scene.gd
@@ -18,5 +18,8 @@ func _ready():
 	Global.connect("blend_shadow_splits_changed", _blend_shadow_splits)
 	Global.emit_signal("blend_shadow_splits_changed")
 	
-	await get_tree().create_timer(2.0).timeout
+	$Ambience.volume_db = -20.0
+	var ambi_tween = create_tween()
+	ambi_tween.tween_property($Ambience, "volume_db", -3.0, 2.0).set_trans(Tween.TRANS_EXPO)
+	await get_tree().create_timer(4.0).timeout
 	$Music.play()
