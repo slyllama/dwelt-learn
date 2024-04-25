06ebd5151d4db2fe4993278182807636dffad3a2
Alex Laing <alex.laing@me.com>
2024-04-25 22:30:57 +1200

    Added visual effects around screen border

---
+++ lib/ui/hud/IntroVFX.gd
@@ -0,0 +1,42 @@
+extends ColorRect
+
+@export var runtime = 0.4
+
+const MAX_ALPHA = 1.0
+const MAX_SIZE = 0.5
+var max_alpha = MAX_ALPHA
+var max_size = MAX_SIZE
+
+# 'at' is a value from 0.0 to 1.0; all parameters should be set off this value
+func _trans_vfx_params(at):
+	material.set_shader_parameter("size", max_size + max_size * at)
+	material.set_shader_parameter("exponent", 1.0 + at * 2.0)
+	material.set_shader_parameter("darkness", (1.0 - at) * 0.5)
+	material.set_shader_parameter("overall_alpha", max_alpha - max_alpha * at)
+
+func transition_vfx_in():
+	var vfx_tween = create_tween()
+	vfx_tween.tween_method(_trans_vfx_params, 1.0, 0.0, runtime).set_trans(Tween.TRANS_CIRC)
+
+# Plays a less intense version of the former
+func transition_subtle_vfx_in():
+	max_alpha = 0.75
+	max_size = 0.8
+	transition_vfx_in()
+
+func transition_vfx_out():
+	var vfx_tween = create_tween()
+	vfx_tween.tween_method(_trans_vfx_params, 0.0, 1.0, runtime)
+	await vfx_tween.finished
+	max_alpha = MAX_ALPHA
+	max_size = MAX_SIZE
+
+func _ready():
+	Global.dialogue_played.connect(transition_subtle_vfx_in.unbind(1))
+	Global.dialogue_closed.connect(transition_vfx_out)
+	Global.dialogue_closed_early.connect(transition_vfx_out)
+	Global.player_position_locked.connect(transition_subtle_vfx_in.unbind(4))
+	Global.player_position_unlocked.connect(transition_vfx_out)
+
+	await get_tree().create_timer(0.6).timeout;
+	transition_vfx_out()
