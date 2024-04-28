9c220686d1ee477b82001deaa549393c1591744c
Alex Laing <alex.laing@me.com>
2024-04-28 15:30:57 +1200

    Implemented threaded loading with screen

---
+++ lib/loading/loading.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+# Loading screen
+
+## If this option is set, opening the loading screen will automatically load
+## the map specified in [code]debug_map[/code]. 
+@export var debug_mode = false
+@export var debug_map = "test_room"
+
+var target_path: String
+var status: int
+var progress: Array[float] # ResourceLoader will put its status details here
+
+func load_map(name):
+	var path = "maps/" + str(name) + "/" + str(name) + ".tscn"
+	print("Loading '" + path + "'")
+	target_path = path
+	ResourceLoader.load_threaded_request(target_path)
+
+func _ready():
+	if debug_mode == true:
+		load_map(debug_map)
+
+func _process(_delta):
+	if target_path == null or target_path == "": return
+	status = ResourceLoader.load_threaded_get_status(target_path, progress)
+	match status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			$LoadBlack/ProgressBar.value = progress[0] * 100
+		ResourceLoader.THREAD_LOAD_LOADED:
+			get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(target_path))
