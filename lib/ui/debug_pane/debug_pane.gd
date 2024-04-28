4a344906456f7630e88c69950babae4919e636ed
Alex Laing <alex.laing@me.com>
2024-04-29 11:54:20 +1200

    Render data debug polling

--- lib/ui/debug_pane/debug_pane.gd
+++ lib/ui/debug_pane/debug_pane.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @export var fps_lower_limit = 20
 
 func _ready():
+	$Render.text = ""
 	Global.debug_toggled.connect(func():
 		visible = Global.debug_state
 		$Settings.visible = Global.debug_state)
@@ -13,8 +14,9 @@ func _input(_event):
 		Global.debug_state = !Global.debug_state
 		Global.debug_toggled.emit()
 
+var i = 0
+
 func _process(_delta):
-	
 	var colour = "green"
 	$Details.text = str(Global.debug_details_text)
 	var fps = Engine.get_frames_per_second()
@@ -22,3 +24,16 @@ func _process(_delta):
 		colour = "red"
 	$FPSCounter.text = ("[color=" + colour + "]"
 		+ str(Engine.get_frames_per_second()) + "fps[/color]")
+	
+	# Only get render profiling data if the debugger is on (this is a little
+	# more expensive). We also don't refresh this every frame
+	if Global.debug_state == true:
+		if i >= 10:
+			var prim = Performance.get_monitor(
+				Performance.RENDER_TOTAL_PRIMITIVES_IN_FRAME)
+			var mem = Performance.get_monitor(
+				Performance.RENDER_BUFFER_MEM_USED)
+			$Render.text = "\nPrimitives: " + str(prim)
+			$Render.text += "\nRender buffer: " + str(int(mem / 1000000)) + "MB"
+			i = 0
+	i += 1
