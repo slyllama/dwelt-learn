c96edc74b3d7fb6fad91afbd927c81ac853e5ff8
Alex Laing <alex.laing@me.com>
2024-04-30 13:20:21 +1200

    Animated anime texture

--- lib/player/Anime.gd
+++ lib/player/Anime.gd
@@ -2,27 +2,29 @@ extends CanvasLayer
 # "Anime" effect which shows when the player moves
 
 @export var time = 0.1
-@export var maximum_alpha = 0.13
+@export var maximum_alpha = 0.2
 
 var active = false
 
+func _set_alpha(a):
+	$AnimeTex.material.set_shader_parameter("modulate_a", a)
+
 func anime_in():
 	active = true
-	$AnimeTex.modulate.a = 0.0
+	$AnimeTex.material.set_shader_parameter("modulate_a", 0.0)
 	$AnimeTex.visible = true
 	var fade_tween = create_tween()
-	fade_tween.tween_property($AnimeTex, "modulate:a", maximum_alpha, time)
+	fade_tween.tween_method(_set_alpha, 0.0, maximum_alpha, time)
 
 func anime_out():
 	active = false
-	$AnimeTex.modulate.a = maximum_alpha
+	$AnimeTex.material.set_shader_parameter("modulate_a", maximum_alpha)
 	var fade_tween = create_tween()
-	fade_tween.tween_property($AnimeTex, "modulate:a", 0.0, time)
+	fade_tween.tween_method(_set_alpha, maximum_alpha, 0.0, time)
 	fade_tween.tween_callback(func():
 		if active == true:
 			return
 		$AnimeTex.visible = false)
 
 func _ready():
-	$AnimeTex.modulate.a = 0.0
 	$AnimeTex.visible = false
