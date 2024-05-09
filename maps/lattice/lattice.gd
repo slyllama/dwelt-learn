1c06ba60eccdb9fa759951707625db32e6b64b77
Alex Laing <alex.laing@me.com>
2024-05-10 11:56:20 +1200

    New player model GLB #6

--- maps/lattice/lattice.gd
+++ maps/lattice/lattice.gd
@@ -12,3 +12,7 @@ func _ready():
 	%Player/CamPivot.new_cam_x_rotation += 24.0
 	%Player/CamPivot.rotation_degrees.y += 10.0
 	%Player/CamPivot.new_cam_y_rotation += 10.0
+	
+	#$Euclid/AnimationPlayer.animation_finished.connect(
+		#func(_anim): $Euclid/AnimationPlayer.play("Fly"))
+	$Euclid/AnimationPlayer.play("Idle")
