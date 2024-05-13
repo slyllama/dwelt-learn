dfb39c16c094b60e5fe6741c035bfd54c066f418
Alex Laing <alex.laing@me.com>
2024-05-13 17:11:06 +1200

    Greybox changes

---
+++ lib/ui/dialogue/dialogue_world/dialogue_world.gd
@@ -0,0 +1,5 @@
+extends SubViewport
+
+func _ready():
+	#TODO: debug
+	$"3DWorld/TestModel/AnimationPlayer".play("Idle")
--- maps/lattice/lattice.gd
+++ maps/lattice/lattice.gd
@@ -3,4 +3,9 @@ extends "res://lib/world_loader/world_loader.gd"
 func _ready():
 	super()
 	$VentFan/AnimationPlayer.play("Fan")
-	$FourierTest/AnimationPlayer.play("Idle")
+	$Path3D/PathFollow3D/FourierTest/AnimationPlayer.play("Idle")
+
+func _process(delta):
+	$Path3D/PathFollow3D.progress += 0.05
+	if $Path3D/PathFollow3D.progress_ratio >= 0.99:
+		$Path3D/PathFollow3D.progress = 0.0
