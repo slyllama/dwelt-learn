86a221f2a940c0d0e7a981935f108f944e1fe6f5
Alex Laing <alex.laing@me.com>
2024-05-14 11:04:35 +1200

    Greybox

--- maps/lattice/lattice.gd
+++ maps/lattice/lattice.gd
@@ -9,3 +9,9 @@ func _process(_delta):
 	$Path3D/PathFollow3D.progress += 0.05
 	if $Path3D/PathFollow3D.progress_ratio >= 0.99:
 		$Path3D/PathFollow3D.progress = 0.0
+
+func _on_fourier_visible_screen_entered():
+	print("fourier IN")
+
+func _on_fourier_visible_screen_exited():
+	print("fourier OUT")
