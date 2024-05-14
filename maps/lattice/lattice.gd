2679ef34cbc02157aca91a757c0adaba63ad72d2
Alex Laing <alex.laing@me.com>
2024-05-14 15:47:27 +1200

    Lattice world building

--- maps/lattice/lattice.gd
+++ maps/lattice/lattice.gd
@@ -3,15 +3,4 @@ extends "res://lib/world_loader/world_loader.gd"
 func _ready():
 	super()
 	$VentFan/AnimationPlayer.play("Fan")
-	$Path3D/PathFollow3D/FourierTest/AnimationPlayer.play("Idle")
-
-func _process(_delta):
-	$Path3D/PathFollow3D.progress += 0.05
-	if $Path3D/PathFollow3D.progress_ratio >= 0.99:
-		$Path3D/PathFollow3D.progress = 0.0
-
-func _on_fourier_visible_screen_entered():
-	print("fourier IN")
-
-func _on_fourier_visible_screen_exited():
-	print("fourier OUT")
+	$FourierTest/AnimationPlayer.play("Idle")
