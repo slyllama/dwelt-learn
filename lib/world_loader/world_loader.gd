1dc7311b43c79a34b8f15c15a14a06b83f7409a2
Alec <alex.laing@me.com>
2024-05-14 23:29:08 +1200

    Dialogue enhancements (#10)
    * #8 proper character model loading
    
    * macOS performance improvements (lighting)
    
    * Greybox
    
    * Improvements to spring arm functioning

--- lib/world_loader/world_loader.gd
+++ lib/world_loader/world_loader.gd
@@ -1,14 +1,19 @@
 extends Node3D
-
-# All lights in here will be excluded from spotlight shadows. Remember to add
-# to this before calling super().
-var exclude_from_shadow = []
-var first_settings_run = false
-
 # This script loads settings, music, and other things into the world.
 # It should be extended after creating a new scene. NOTE: 'super()' MUST
 # called from the inheriting script in order for everything to be loaded
 # properly.
+
+# All lights in here will be excluded from spotlight shadows. Remember to add
+# to this before calling super().
+var exclude_from_shadow = []
+# Static bodies included as children of the objects listed in
+# 'spring_arm_objects' will have collision mask bit (2) set, and the player's
+# spring arm will prevent the camera from clipping through them. Remember to
+# add to this before calling super().
+var spring_arm_objects = []
+
+var first_settings_run = false
 
 func _setting_changed(get_setting_id):
 	if first_settings_run == false: first_settings_run = true
@@ -34,6 +39,15 @@ func _ready():
 	Global.setting_changed.connect(_setting_changed)
 	for setting in Global.settings:
 		if !setting == "volume": Global.setting_changed.emit(setting)
+	
+	var col_count = 0
+	for o in spring_arm_objects:
+		for n in o.get_children():
+			if n is StaticBody3D:
+				col_count += 1
+				n.set_collision_layer_value(2, true)
+		if col_count > 0: print("[" + str(o) + "] setting spring-arm collision mask for "
+			+ str(col_count) + " object(s)")
 	
 	var fade_bus_in = create_tween()
 	fade_bus_in.tween_method(Utilities.set_master_vol, 0.0, Global.settings.volume, 1.5)
--- maps/lattice/lattice.gd
+++ maps/lattice/lattice.gd
@@ -1,6 +1,8 @@
 extends "res://lib/world_loader/world_loader.gd"
 
 func _ready():
+	spring_arm_objects = [$Greybox]
+	
 	super()
 	$VentFan/AnimationPlayer.play("Fan")
 	$FourierTest/AnimationPlayer.play("Idle")
