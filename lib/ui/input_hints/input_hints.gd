6994f06e33c80509e2aab404b919d77082cc02e8
Alex Laing <alex.laing@me.com>
2024-05-27 09:53:20 +1200

    Fixed simultaneous input hints closes #43

--- lib/ui/input_hints/input_hints.gd
+++ lib/ui/input_hints/input_hints.gd
@@ -5,15 +5,8 @@ var card_nodes = []
 var transitioning = false
 var active = false
 
-# { "data": Array, "clear_time": float }
-var buffered_data = {}
-var buffered_close = false
-
 func clear_hints():
-	if !buffered_close: return # for late events
 	active = false
-	buffered_close = false
-	
 	for c in card_nodes.size():
 		var cn = card_nodes[card_nodes.size() - 1 -c] # reverse
 		if active == false:
@@ -36,32 +29,15 @@ func show_hints(get_card_data, clear_time = 0.0):
 		card_node.description = card["description"]
 		card_node.key_text = card["key"]
 		$Container.add_child(card_node)
-		
 		card_nodes.append(card_node)
 	
 	if clear_time > 0.0:
 		$ClearTimer.set_wait_time(clear_time)
 		$ClearTimer.start()
-	
-	buffered_data = {}
 
 func _ready():
-	Global.input_hint_played.connect(func(get_data, get_clear_time):
-		buffered_data = { "data": get_data, "clear_time": get_clear_time }
-	)
-	Global.input_hint_cleared.connect(func():
-		buffered_close = true
-	)
+	Global.input_hint_played.connect(show_hints)
+	Global.input_hint_cleared.connect(clear_hints)
 
 func _on_clear_timer_timeout():
-	buffered_close = true
-
-var b = false
-
-func _buffer_process():
-	b = !b
-	if buffered_close and active and b:
-		clear_hints()
-		return
-	if buffered_data != {} and !active:
-		show_hints(buffered_data.data, buffered_data.clear_time)
+	clear_hints()
